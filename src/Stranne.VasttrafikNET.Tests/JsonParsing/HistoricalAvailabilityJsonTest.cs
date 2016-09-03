24d9cdad023c8f2c07161d605f545d43202e5208
Olof Stranne <olof.stranne@outlook.com>
2016-09-04 00:48:04 +0200

    Fix bug with test cases not working with different timezone
    Historical availability on Commuter Parking Service returns a date with a specified timezone. Its unit test now handle if the machine running the test has a different timezone than W. Europe Standard Time.

--- src/Stranne.VasttrafikNET.Tests/JsonParsing/HistoricalAvailabilityJsonTest.cs
+++ src/Stranne.VasttrafikNET.Tests/JsonParsing/HistoricalAvailabilityJsonTest.cs
@@ -13,15 +13,15 @@ namespace Stranne.VasttrafikNET.Tests.JsonParsing
         public static TheoryData TestParameters => new TheoryData<string, object>
         {
             { "", 5 },
-            { "[0].DateTime", new DateTime(2016, 8, 1, 8, 0, 0) },
+            { "[0].DateTime", TimeZoneInfo.ConvertTime(new DateTime(2016, 8, 1, 8, 0, 0), TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")) },
             { "[0].FreeSpaces",79 },
-            { "[1].DateTime", new DateTime(2016, 8, 1, 8, 15, 0) },
+            { "[1].DateTime", TimeZoneInfo.ConvertTime(new DateTime(2016, 8, 1, 8, 15, 0), TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")) },
             { "[1].FreeSpaces", 78 },
-            { "[2].DateTime", new DateTime(2016, 8, 1, 8, 30, 0) },
+            { "[2].DateTime", TimeZoneInfo.ConvertTime(new DateTime(2016, 8, 1, 8, 30, 0), TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")) },
             { "[2].FreeSpaces", 71 },
-            { "[3].DateTime", new DateTime(2016, 8, 1, 8, 45, 0) },
+            { "[3].DateTime", TimeZoneInfo.ConvertTime(new DateTime(2016, 8, 1, 8, 45, 0), TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")) },
             { "[3].FreeSpaces", 70 },
-            { "[4].DateTime", new DateTime(2016, 8, 1, 9, 0, 0) },
+            { "[4].DateTime", TimeZoneInfo.ConvertTime(new DateTime(2016, 8, 1, 9, 0, 0), TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")) },
             { "[4].FreeSpaces", 67 }
         };
 
