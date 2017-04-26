caf55797e01f44a8addeee6820b1b174b71e174a
Olof Stranne <olof.stranne@gmail.com>
2017-04-26 23:01:25 +0200

    Add powershell script to generate local coverage report

---
+++ GenerateCoverageReport.ps1
@@ -0,0 +1,29 @@
+$rootPath = (Get-Item -Path ".\" -Verbose).FullName
+
+dotnet restore
+
+Set-Location 'src\Stranne.VasttrafikNET.Tests'
+
+$resultsFile = $rootPath + '\artifacts\Stranne.VasttrafikNET_coverage.xml'
+$openCoverConsole = $ENV:USERPROFILE + '\.nuget\packages\OpenCover\4.6.690\tools\OpenCover.Console.exe'
+$target = '-target:C:\Program Files\dotnet\dotnet.exe'
+$targetArgs = '-targetargs: test -c Release'
+$filter = '-filter:+[Stranne.VasttrafikNET]* -[Stranne.VasttrafikNET.Tests]*'
+$output = '-output:' + $resultsFile
+
+& $openCoverConsole $target $targetArgs '-register:user' $filter '-hideskipped:Filter' '-returntargetcode' '-mergeoutput' $output '-oldStyle'
+
+Set-Location $rootPath
+
+$reportGenerator = $ENV:USERPROFILE + '\.nuget\packages\reportgenerator\2.5.6\tools\ReportGenerator.exe'
+$reportOutput = $rootPath + '\artifacts\CoverageReport\'
+$reports = '-reports:' + $resultsFile
+$targetdir = '-targetdir:' + $reportOutput
+
+If (Test-Path $reportOutput){
+	Remove-Item $reportOutput -recurse
+}
+
+& $reportGenerator $reports $targetdir
+
+Invoke-Item ($reportOutput + 'index.htm')
