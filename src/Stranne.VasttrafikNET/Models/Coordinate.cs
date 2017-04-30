fbfadf5cf46f1e1a1e1fedf9a94655bde1190dcf
Olof Stranne <olof.stranne@gmail.com>
2017-04-30 11:53:45 +0200

    Add constructor with parameters for coordinate
    Make it possible to create a defined coordinate with less code.

--- src/Stranne.VasttrafikNET/Models/Coordinate.cs
+++ src/Stranne.VasttrafikNET/Models/Coordinate.cs
@@ -14,5 +14,22 @@
         /// Longitude in WGS84 decimal form
         /// </summary>
         public double Longitude { get; set; }
+
+        /// <summary>
+        /// Create a new coordinate.
+        /// </summary>
+        public Coordinate()
+        { }
+
+        /// <summary>
+        /// Create a coordinate with latitude and longitude pre defined.
+        /// </summary>
+        /// <param name="latitude">Latitude in WGS84 decimal form</param>
+        /// <param name="longitude">Longitude in WGS84 decimal form</param>
+        public Coordinate(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
     }
 }
